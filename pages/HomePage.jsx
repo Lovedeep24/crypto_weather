f6994ef7a100330529026ede08fcf831749c4bcd
Lovedeep <lovedeep24253@gmail.com>
2025-04-04 12:17:30 +0530

    created homepage

---
+++ pages/HomePage.jsx
@@ -0,0 +1,68 @@
+"use client";
+import React from 'react'
+import { WorldMap } from "@/components/ui/world-map";
+import { motion } from "framer-motion";
+import RotatingText from '@/components/ui/rotatingText';
+export default function HomePage() {
+  return (
+    <div className=" border-2 border-red-900 dark:bg-black bg-white w-full">
+      <div className="max-w-7xl mx-auto text-center bg-black">
+      <div className='flex border-2 items-center justify-center gap-3 w-full border-black'>
+        <p className='text-3xl text-white font-extrabold'>Creative</p>
+        <RotatingText
+          texts={['React', 'Bits', 'Is', 'Cool!']}
+          mainClassName="px-2 w-[10%]  sm:px-2 text-xl md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+          staggerFrom={"last"}
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          exit={{ y: "-120%" }}
+          staggerDuration={0.025}
+          splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+          transition={{ type: "spring", damping: 30, stiffness: 400 }}
+          rotationInterval={2000}
+        />
+        </div>
+        <p className="text-sm md:text-lg text-neutral-500 max-w-2xl mx-auto py-4">
+          Break free from traditional boundaries. Work from anywhere, at the
+          comfort of your own studio apartment. Perfect for Nomads and
+          Travellers.
+        </p>
+      </div>
+      <WorldMap
+        dots={[
+          {
+            start: {
+              lat: 64.2008,
+              lng: -149.4937,
+            }, // Alaska (Fairbanks)
+            end: {
+              lat: 34.0522,
+              lng: -118.2437,
+            }, // Los Angeles
+          },
+          {
+            start: { lat: 64.2008, lng: -149.4937 }, // Alaska (Fairbanks)
+            end: { lat: -15.7975, lng: -47.8919 }, // Brazil (Brasília)
+          },
+          {
+            start: { lat: -15.7975, lng: -47.8919 }, // Brazil (Brasília)
+            end: { lat: 38.7223, lng: -9.1393 }, // Lisbon
+          },
+          {
+            start: { lat: 51.5074, lng: -0.1278 }, // London
+            end: { lat: 28.6139, lng: 77.209 }, // New Delhi
+          },
+          {
+            start: { lat: 28.6139, lng: 77.209 }, // New Delhi
+            end: { lat: 43.1332, lng: 131.9113 }, // Vladivostok
+          },
+          {
+            start: { lat: 28.6139, lng: 77.209 }, // New Delhi
+            end: { lat: -1.2921, lng: 36.8219 }, // Nairobi
+          },
+        ]}
+      />
+    </div>
+  );
+
+}
