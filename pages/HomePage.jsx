e3c6bbbfe8c7bfdf8d38179374e483147dcd2518
Lovedeep <lovedeep24253@gmail.com>
2025-04-04 13:55:18 +0530

    done with dashboard UI

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,17 +1,22 @@
 "use client";
 import React from 'react'
-import { WorldMap } from "@/components/ui/world-map";
-import { motion } from "framer-motion";
+import { Boxes } from "@/components/ui/background-boxes"
+import { cn } from "@/lib/utils";
 import RotatingText from '@/components/ui/rotatingText';
+import { ButtonColorful } from "@/components/ui/button-colorful"
+import { Button } from "@/components/ui/button"
+import { LucideIcon } from "lucide-react"
+
 export default function HomePage() {
   return (
-    <div className=" border-2 border-red-900 dark:bg-black bg-white w-full">
-      <div className="max-w-7xl mx-auto text-center bg-black">
-      <div className='flex border-2 items-center justify-center gap-3 w-full border-black'>
-        <p className='text-3xl text-white font-extrabold'>Creative</p>
+    <div className="h-screen relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center">
+      <div className="absolute  h-full  bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+      <Boxes />
+      <div className='flex items-center justify-center gap-3 relative z-10'>
+        <p className=' text-2xl md:text-5xl text-white font-semibold text-nowrap'>Get Latest Information on </p>
         <RotatingText
-          texts={['React', 'Bits', 'Is', 'Cool!']}
-          mainClassName="px-2 w-[10%]  sm:px-2 text-xl md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+          texts={['Weather', 'Crypto', 'Bitcoin', 'Stocks', 'NFTs', 'Latest News']}
+          mainClassName="px-2  sm:px-2 text-2xl font-semibold md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 md:mt-2 mt-0 justify-center rounded-lg"
           staggerFrom={"last"}
           initial={{ y: "100%" }}
           animate={{ y: 0 }}
@@ -22,46 +27,10 @@ export default function HomePage() {
           rotationInterval={2000}
         />
         </div>
-        <p className="text-sm md:text-lg text-neutral-500 max-w-2xl mx-auto py-4">
-          Break free from traditional boundaries. Work from anywhere, at the
-          comfort of your own studio apartment. Perfect for Nomads and
-          Travellers.
-        </p>
-      </div>
-      <WorldMap
-        dots={[
-          {
-            start: {
-              lat: 64.2008,
-              lng: -149.4937,
-            }, // Alaska (Fairbanks)
-            end: {
-              lat: 34.0522,
-              lng: -118.2437,
-            }, // Los Angeles
-          },
-          {
-            start: { lat: 64.2008, lng: -149.4937 }, // Alaska (Fairbanks)
-            end: { lat: -15.7975, lng: -47.8919 }, // Brazil (Brasília)
-          },
-          {
-            start: { lat: -15.7975, lng: -47.8919 }, // Brazil (Brasília)
-            end: { lat: 38.7223, lng: -9.1393 }, // Lisbon
-          },
-          {
-            start: { lat: 51.5074, lng: -0.1278 }, // London
-            end: { lat: 28.6139, lng: 77.209 }, // New Delhi
-          },
-          {
-            start: { lat: 28.6139, lng: 77.209 }, // New Delhi
-            end: { lat: 43.1332, lng: 131.9113 }, // Vladivostok
-          },
-          {
-            start: { lat: 28.6139, lng: 77.209 }, // New Delhi
-            end: { lat: -1.2921, lng: 36.8219 }, // Nairobi
-          },
-        ]}
-      />
+      <h1 className={cn("md:text-3xl text-xl text-white mt-5 relative z-20")}>
+        Your one stop solution for all your tracking needs and daily updates
+      </h1>
+     <ButtonColorful className={"mt-10"} />
     </div>
   );
 
